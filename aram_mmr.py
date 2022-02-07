668874cfb584d8f6f25cbe3546ec3fd12294d321
christoph.henning <christoph.henning@tu-braunschweig.de>
2022-02-07 16:49:25 +0100

    first version of aram bot

---
+++ aram_mmr.py
@@ -0,0 +1,41 @@
+import requests
+"""
+This project is based on the API by: https://euw.whatismymmr.com/.
+Documentation and licencing can be found on: https://dev.whatismymmr.com/#license.
+"""
+
+
+def get_data(name, region):
+    error = None
+    if name == "":
+        error = 200
+        return None, None, None, error
+    url = f"https://{region}.whatismymmr.com/api/v1/summoner?name={name}"
+    data = requests.get(url).json()
+    if "error" in data:
+        error = data["error"]["code"]
+        return None, None, None, error
+    aram_data = data["ARAM"]
+    mmr = aram_data["avg"]
+    deviation = aram_data["err"]
+    rank = aram_data["closestRank"]
+    return mmr, deviation, rank, error
+
+
+def get_mmr(name, region):
+    mmr, deviation, rank, err = get_data(name, region)
+    # error on the API's part
+    if err == 0 or err == 1:
+        response = "Unexpected Error."
+    # no name was given
+    elif err == 200:
+        response = "Try using !mmr ign server. The server is EUW by default."
+    elif err == 100:
+        response = f"Summoner {name} was not found."
+    elif err == 101 or mmr is None or rank is None:
+        response = f"No recent data for {name}."
+    elif err == 9001:
+        response = "Too many requests. Please try again later."
+    else:
+        response = f"{name} has an ARAM mmr of {mmr} ±{deviation}, which is equal to {rank}."
+    return response, rank
---
+++ main.py
@@ -0,0 +1,53 @@
+import os
+import discord
+import time
+import aram_mmr
+from keep_alive import keep_alive
+
+# set token for discord bot
+TOKEN = os.environ['TOKEN']
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print("Logged in as {0.user}".format(client))
+
+
+@client.event
+async def on_message(message):
+    time.sleep(1)
+    # username = str(message.author).split('#')[0]
+    user_message = str(message.content)
+    # print(f"{username}: {user_message}")
+
+    # bot doesn't respond to own message. Just for safety.
+    if message.author == client.user:
+        return
+    split_msg = user_message.split()
+    # detect mmr command
+    if split_msg[0] == '!mmr':
+        last = split_msg[len(split_msg)-1].lower()
+        # set default region to euw
+        region = "euw"
+        name = ""
+        # check if specific region is present
+        if last == "euw" or last == "eune" or last == "na":
+            region = last
+            # remove region from string
+            split_msg.pop()
+        # construct name if it includes spaces
+        for i in range(1, len(split_msg)):
+            name = name + " " + split_msg[i]
+        response, rank = aram_mmr.get_mmr(name, region)
+        # print message and corresponding image
+        if rank is None:
+          await message.channel.send(response)
+        else:
+          await message.channel.send(response, file=discord.File("images/" + rank.lower().split(" ")[0] + ".webp"))
+        return
+
+# web server
+keep_alive()
+client.run(TOKEN)
