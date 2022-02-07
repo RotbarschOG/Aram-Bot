52e0dddbeedb7820870f7b629bcaeafa68d820e1
christoph.henning <christoph.henning@tu-braunschweig.de>
2022-02-07 19:42:38 +0100

    added support for kr, removed double whitespace bug and made error messages more helpful

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import aram_mmr
 from keep_alive import keep_alive
 
 # set token for discord bot
-TOKEN = os.environ['TOKEN']
+TOKEN = os.environ["TOKEN"]
 
 client = discord.Client()
 
@@ -18,6 +18,8 @@ async def on_ready():
 @client.event
 async def on_message(message):
     time.sleep(1)
+    if message.channel.name != "aram-scoreboard":
+        return
     # username = str(message.author).split('#')[0]
     user_message = str(message.content)
     # print(f"{username}: {user_message}")
@@ -27,25 +29,26 @@ async def on_message(message):
         return
     split_msg = user_message.split()
     # detect mmr command
-    if split_msg[0] == '!mmr':
+    if split_msg[0] == "!mmr":
         last = split_msg[len(split_msg)-1].lower()
         # set default region to euw
         region = "euw"
         name = ""
         # check if specific region is present
-        if last == "euw" or last == "eune" or last == "na":
+        if last == "euw" or last == "eune" or last == "na" or last == "kr":
             region = last
             # remove region from string
             split_msg.pop()
         # construct name if it includes spaces
         for i in range(1, len(split_msg)):
             name = name + " " + split_msg[i]
+        name = name.lstrip()
         response, rank = aram_mmr.get_mmr(name, region)
         # print message and corresponding image
         if rank is None:
-          await message.channel.send(response)
+            await message.channel.send(response)
         else:
-          await message.channel.send(response, file=discord.File("images/" + rank.lower().split(" ")[0] + ".webp"))
+            await message.channel.send(response, file=discord.File("images/" + rank.lower().split(" ")[0] + ".webp"))
         return
 
 # web server
